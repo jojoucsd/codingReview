8a92aeba9d232c7c7e8a68f14f5b23efbf9d2930
Ling Lu <jojoucsd@gmail.com>
2020-09-03 16:42:57 -0700

    levenshteinDistance

---
+++ dynamicProgramming/levenshteinDistance.js
@@ -0,0 +1,69 @@
+/*
+Write a function that takes in two strings and returns the min number of edit operations that need
+to be performed on the first string to obtain the second string.
+
+there are 3 edit operations: insertion of a char, deletion of a char, and sub of a char
+for another
+
+Sample Input
+"str1": "abc", "str2": "yabd"
+Output
+2 // insert "y" sub "c" for "d"
+*/
+
+levenshteinDistance = (str1, str2) =>{
+    const edits =[]
+    for (let i =0; i<str2.length+1 ;i++){
+        const row = []
+        for (let j = 0; j < str1.length+1; j++){
+            row.push(j)
+        }
+        row[0] = i
+        edits.push(row)
+    }
+    for (let i = 1; i< str2.length + 1; i++){
+        for (let j = 1 ; j <str1.length + 1; j ++){
+            if( str2[i-1] === str1[j-1]){
+                edits[i][j] = edits[i-1][j-1]
+            }else{
+                edits[i][j] = 1 + Math.min(edits[i-1][j-1], edits[i-1][j], edits[i][j-1])
+            }
+        }
+    }
+    // console.log(edits)
+    return coins[str2.length][str1.length]
+}
+
+levenshteinDistance2 =(str1, str2) =>{
+    const small = str1.length < str2.length ? str1:str2
+    const big = str1.length >= str2.length ? str1:str2
+    const evenEdits = []
+    const oddEdits = new Array(small.length + 1);
+
+    for (let j = 0 ; j<small.length+1; j++){
+        evenEdits.push(j)
+    }
+    for (let i = 1; i < big.length +1 ; i++){
+        let currentEdits, previousEdits;
+        if (i%2 === 1){
+            currentEdits= oddEdits
+            previousEdits = evenEdits
+        }else{
+            currentEdits= evenEdits
+            previousEdits=oddEdits
+        }
+        currentEdits[0] = i
+        for( let j = 1 ; j<small.length+1; j++){
+            if (big[i-1] === small[j-1]){
+                currentEdits[j] = previousEdits[j-1]
+            }else{
+                currentEdits[j] = 1 + Math.min(previousEdits[j-1], previousEdits[j],currentEdits[j-1])
+            }        
+        }
+    }
+    // console.log(evenEdits, oddEdits)
+    return big.length % 2 === 0 ? evenEdits[small.length] : oddEdits[small.length]
+}
+
+// levenshteinDistance("abc", "yabd")
+console.log(levenshteinDistance2("abc", "yabd"))
