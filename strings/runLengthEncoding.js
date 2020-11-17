6c9ccd776b0df88fb882fb7c577a4e86b513b29b
Ling Lu <jojoucsd@gmail.com>
2020-11-17 14:17:13 -0800

    strings encode problem added

---
+++ strings/runLengthEncoding.js
@@ -0,0 +1,60 @@
+/*
+Write a function that takes in a non-empty string and return its run-length encoding.
+
+From Wikipedia, "run-length" encoding is a form of lossless data compression in which runs of data are stored as a single data value and 
+count, rather than as the original run."  For this problem , a run of data is any sequence of consecutive, identical characters. So the run
+"AAA" would be run-length-encoded as "3A".
+
+To make things more complicated, however, the input string can contain all sorts of special characters, including numbers. And since encoded
+data must be decodable, this means that we can't naively run-length-encode long runs. For example,  the run "AAAAAAAAAAAA" (12 As), can't 
+naively be encoded as "12A", since this string can be decoded as either "AAAAAAAAAAAA" or "1AA". Thus, long runs(runs of 10 or more characters)
+should be encoded in a split fashion; the aforementioned run should be encoded as "9A3A".
+
+Sample Input
+string = "AAAAAAAAAAAAABBCCCCDD"
+
+Sample Output
+"9A4A2B4C2D"
+*/
+
+
+const inputStr = "AAAAAAAAAAAAABBCCCCDD"
+
+
+runLengthEncodingSolution = (str) =>{
+    const result = []
+    let currentRunLength = 1
+
+    for (let i = 1; i < str.length; i++){
+        const currentChar = str[i]
+        const prevChar = str[i-1]
+
+        if (currentChar !== prevChar || currentRunLength === 9){
+            result.push(currentRunLength.toString())
+            result.push(prevChar)
+            currentRunLength =0
+        }
+        currentRunLength++
+    }
+    result.push(currentRunLength.toString())
+    result.push(str[str.length - 1])
+    return result.join("")
+}
+
+
+function runLengthEncoding(string) {
+    // Write your code here.
+      let result = ""
+      let count = 1
+      for (let i = 1 ; i < string.length+1; i++ ){
+          let currentValue = string[i]
+          let prevValue = string[i-1]
+          if (count ===9 || string[i] !== string[i-1]){
+              result +=count + string[i-1]
+              count = 0
+          }
+          count++
+      }
+      return result
+  }
+console.log(runLengthEncodingSolution(inputStr))
